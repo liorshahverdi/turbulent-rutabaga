5c1d8b596308ce4e015d460c87ae352ec05bbeff
LiorUbuntu <liorshahverdi@gmail.com>
2015-08-07 20:23:44 -0700

    i guess itsuseful

--- learning-qt4/msgbox.py
+++ learning-qt4/msgbox.py
@@ -12,13 +12,22 @@ w = QWidget()
  
 # Show a message box
 result = QMessageBox.question(w, 'Message', "Do you like Python?", QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+
+# Just a warning
+warning = QMessageBox.warning(w, "Message", "Are you sure you didn't want butter?")
+
+# Information box
+infaaa = QMessageBox.information(w,"message", "An informational message")
+
+criti = QMessageBox.critical(w,"message", "But like seriously, this is bad. You sure you wanna do this?")
+
+ab = QMessageBox.about(w, "About", "An example messagebox @ pythonspot.com ")
  
 if result == QMessageBox.Yes:
     print 'Yes.'
 else:
-    print 'No.'        
- 
- 
+    print 'No.'
+
 # Show window
 w.show() 
  
